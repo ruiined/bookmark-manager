ff7791a5e3bed52e3c92e3ac5107b17bc1a307f2
ruiined <ruiined@gmail.com>
2022-03-15 11:40:02 +0000

    added an image of a diagram
    Co-authored-by: Rob Oman <r94o@users.noreply.github.com>

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -5,9 +5,5 @@ describe Bookmarks do
     it "returns an array of bookmarks" do
       expect(described_class.all).to be_an_instance_of(Array)
     end
-
-    # it "returns url from bookmarks" do
-    #   expect(described_class.all).to output (link).to_stdout
-    # end
   end
 end
