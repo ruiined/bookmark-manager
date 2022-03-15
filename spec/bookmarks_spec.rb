406f1c89c0a31a8f5ef09274383fc181e25ad4c9
ruiined <ruiined@gmail.com>
2022-03-15 15:40:33 +0000

    got the tests working with a testing invironment
    Co-authored-by: Rob Oman <r94o@users.noreply.github.com>

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -3,9 +3,17 @@
 require 'bookmarks'
 
 describe Bookmarks do
+  let(:link) { "http://testing_bookmarks.com" }
+  
   describe '.all' do
     it 'returns an array of bookmarks' do
       expect(described_class.all).to be_an_instance_of(Array)
+    end
+  end
+  describe '.add' do
+    it 'adds to the test database' do
+      described_class.add(link)
+      expect(described_class.all).to include link
     end
   end
 end
