51921a7e6cb7e497b414ad20462ea75f003ddaf3
ruiined <ruiined@gmail.com>
2022-03-15 11:26:23 +0000

    got the feature test working
    Co-authored-by: Rob Oman
    <r94o@users.noreply.github.com>

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -1,6 +1,6 @@
 feature "testing bookmark manager" do
   scenario  "lists all bookmarks" do
     visit '/bookmarks'
-    expect(page).to have_content("http://www.makersacademy.com")
+    expect(page).to have_content("http://makersacademy.com")
   end
 end
